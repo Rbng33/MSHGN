33534b10b5d9c1ca08852d0bee1bffa82ecc34f5
Benghanem Abderraouf <la_benghanem@esi.dz>
2026-04-12 20:47:56 +0100

    Add Dataset_ETT_minute class for data handling

--- mshgn/data.py
+++ mshgn/data.py
@@ -277,3 +277,89 @@ class Dataset_ETT_hour(Dataset):
 
     def inverse_transform(self, data):
         return self.scaler.inverse_transform(data)
+
+
+
+
+class Dataset_ETT_minute(Dataset):
+    def __init__(self, root_path, flag='train', size=None,
+                 features='S', data_path='ETTm1.csv',
+                 target='OT', scale=True, inverse=False, timeenc=0, freq='t', cols=None):
+        if size is None:
+            self.seq_len = 24 * 4 * 4
+            self.label_len = 24 * 4
+            self.pred_len = 24 * 4
+        else:
+            self.seq_len = size[0]
+            self.label_len = size[1]
+            self.pred_len = size[2]
+
+        assert flag in ['train', 'test', 'val']
+        type_map = {'train': 0, 'val': 1, 'test': 2}
+        self.set_type = type_map[flag]
+
+        self.features = features
+        self.target = target
+        self.scale = scale
+        self.inverse = inverse
+        self.timeenc = timeenc
+        self.freq = freq
+        self.root_path = root_path
+        self.data_path = data_path
+        self.__read_data__()
+
+    def __read_data__(self):
+        self.scaler = StandardScaler()
+        df_raw = pd.read_csv(os.path.join(self.root_path, self.data_path))
+
+        # ETTm1/ETTm2: 15-min frequency → 4× more points than ETTh
+        # Total: ~69,680 rows → splits: 12*30*24*4 / 4*30*24*4 / 4*30*24*4
+        border1s = [0,
+                    12 * 30 * 24 * 4 - self.seq_len,
+                    12 * 30 * 24 * 4 + 4 * 30 * 24 * 4 - self.seq_len]
+        border2s = [12 * 30 * 24 * 4,
+                    12 * 30 * 24 * 4 + 4 * 30 * 24 * 4,
+                    12 * 30 * 24 * 4 + 8 * 30 * 24 * 4]
+
+        border1 = border1s[self.set_type]
+        border2 = border2s[self.set_type]
+
+        if self.features in ('M', 'MS'):
+            cols_data = df_raw.columns[1:]
+            df_data = df_raw[cols_data]
+        else:
+            df_data = df_raw[[self.target]]
+
+        if self.scale:
+            train_data = df_data[border1s[0]:border2s[0]]
+            self.scaler.fit(train_data.values)
+            data = self.scaler.transform(df_data.values)
+        else:
+            data = df_data.values
+
+        df_stamp = df_raw[['date']][border1:border2]
+        df_stamp['date'] = pd.to_datetime(df_stamp.date)
+        data_stamp = time_features(df_stamp, timeenc=self.timeenc, freq=self.freq)
+
+        self.data_x = data[border1:border2]
+        self.data_y = df_data.values[border1:border2] if self.inverse else data[border1:border2]
+        self.data_stamp = data_stamp
+
+    def __getitem__(self, index):
+        s_begin = index
+        s_end = s_begin + self.seq_len
+        r_begin = s_end - self.label_len
+        r_end = r_begin + self.label_len + self.pred_len
+
+        seq_x = self.data_x[s_begin:s_end]
+        seq_y = (np.concatenate([self.data_x[r_begin:r_begin + self.label_len],
+                                  self.data_y[r_begin + self.label_len:r_end]], 0)
+                 if self.inverse else self.data_y[r_begin:r_end])
+        return seq_x, seq_y, self.data_stamp[s_begin:s_end], self.data_stamp[r_begin:r_end]
+
+    def __len__(self):
+        return len(self.data_x) - self.seq_len + 1
+
+    def inverse_transform(self, data):
+        return self.scaler.inverse_transform(data)
+
